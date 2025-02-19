c15a4161bdb1bf421490017db9473b8256204b55
Matthias Engelien <matthias.engelien@sigmatechnology.com>
2025-02-19 14:42:33 +0100

    some zsh config

--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -14,53 +14,18 @@
       ggl = "git pull";
       ggp = "git push";
 
-      # kubectl
-      k = "kubectl";
-      kgno = "kubectl get node";
-      kdno = "kubectl describe node";
-      kgp = "kubectl get pods";
-      kep = "kubectl edit pods";
-      kdp = "kubectl describe pods";
-      kdelp = "kubectl delete pods";
-      kgs = "kubectl get svc";
-      kes = "kubectl edit svc";
-      kds = "kubectl describe svc";
-      kdels = "kubectl delete svc";
-      kgi = "kubectl get ingress";
-      kei = "kubectl edit ingress";
-      kdi = "kubectl describe ingress";
-      kdeli = "kubectl delete ingress";
-      kgns = "kubectl get namespaces";
-      kens = "kubectl edit namespace";
-      kdns = "kubectl describe namespace";
-      kdelns = "kubectl delete namespace";
-      kgd = "kubectl get deployment";
-      ked = "kubectl edit deployment";
-      kdd = "kubectl describe deployment";
-      kdeld = "kubectl delete deployment";
-      kgsec = "kubectl get secret";
-      kdsec = "kubectl describe secret";
-      kdelsec = "kubectl delete secret";
-
-      ld = "lazydocker";
-      lg = "lazygit";
-
-      repo = "cd $HOME/Documents/repositories";
-      temp = "cd $HOME/Downloads/temp";
-
-      v = "nvim";
-      vi = "nvim";
-      vim = "nvim";
-
       ls = "eza --icons always"; # default view
       ll = "eza -bhl --icons --group-directories-first"; # long list
       la = "eza -abhl --icons --group-directories-first"; # all list
       lt = "eza --tree --level=2 --icons"; # tree
+      
+      # Nix
+      nupdate = "nix update flakes"
+      nswitch = "sudo nixos-rebuild switch --flake ~/nixos-config/"
+      hmswitch = "home-manager switch --flake ~/nixos-config/"
     };
     initExtra = ''
-      # kubectl auto-complete
-      source <(kubectl completion zsh)
-
+      
       # bindings
       bindkey -e
       bindkey '^H' backward-delete-word
