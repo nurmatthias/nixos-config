11bf5fb7e85eaa19341b9ef309838c82aff9b4d8
nurMatthias <git@engelien.info>
2025-02-20 10:29:05 +0100

    remove typo

--- modules/nixos/desktop/kde/default.nix
+++ modules/nixos/desktop/kde/default.nix
@@ -32,7 +32,6 @@ in {
     (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
       [General]
       background=${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/MilkyWay/contents/images/5120x2880.png
-      #background = "${background-package}"
     '')
   ];
 }
