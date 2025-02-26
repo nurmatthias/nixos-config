4a78a6827bb21e4b597011f128546417107c5ebc
Matthias Engelien <matthias.engelien@sigmatechnology.com>
2025-02-26 11:53:13 +0100

    set cursor theme

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,6 +1,12 @@
 ## modules/stylix.nix
 { config, pkgs, ... }:
+
+let
+  opacity = 0.95;
+  fontSize = 11;
+in
 {
+  
   stylix.enable = true;
   stylix.image = ../files/wallpaper/wallpaper.jpg;
   stylix.polarity = "dark";
@@ -25,5 +31,23 @@
       package = pkgs.noto-fonts-emoji;
       name = "Noto Color Emoji";
     };
+    
+    sizes = {
+      applications = fontSize;
+      desktop = fontSize;
+      popups = fontSize;
+      terminal = fontSize;
+    };
+  };
+  
+  cursor = {
+    package = pkgs.apple-cursor;
+    name = "apple_cursor";
+    size = 22;
+  };
+  
+  stylix.opacity = {
+    terminal = opacity;
+    popups = opacity;
   };
 }
