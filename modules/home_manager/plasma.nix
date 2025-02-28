646dbb300cd37c3f88bedbf68f0d017c8e795ee0
installer <installer@gmail.com>
2025-02-28 19:33:47 +0100

    add polonium to plasma

--- modules/home_manager/plasma.nix
+++ modules/home_manager/plasma.nix
@@ -13,5 +13,20 @@
       TerminalApplication = "kitty";
       TerminalService = "kitty.desktop";
     };
+
+    kwin = {
+      edgeBarrier = 0; # Disables the edge-barriers introduced in plasma 6.1
+      cornerBarrier = false;
+
+      scripts.polonium.enable = true;
+    };
+
+    configFile = {
+      kwinrc.Desktops.Number = {
+        value = 4;
+        # Forces kde to not change this value (even through the settings app).
+        immutable = true;
+      };
+    };
   };
 }
